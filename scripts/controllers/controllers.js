b6d6b30913a983ca383aeab3c4992502f0f329a4
Erika Moller <erikamoller@gmail.com>
2015-02-18 22:11:12 -0600

    Made contact form, styling is unfinished, the error messages display correctly, but it does not not show a login success page

--- scripts/controllers/controllers.js
+++ scripts/controllers/controllers.js
@@ -1,15 +1,56 @@
 angular.module('basic.controllers', []) 
 .controller('loadingCtrl', function($scope) {
 
+	$scope.sumbitPage = true;
+
 	$scope.submitForm = function() {
-		if($scope.userName === 'erika') {
-			console.log('erika');
-			
-		} else if($scope.userName === '') {
-			console.log('no name');
-			$scope.noName = true;
+
+		var errorArray = [
+			'Name cannot be empty',
+			'Email cannot be empty',
+			'Email must contain an "@"',
+			'Website must start with http://',
+			'Message cannot be left empty'
+		];
+	
+		if($scope.userName === '' || angular.isUndefined($scope.userName)) {
+			// console.log('empty name');
+			$scope.noNameError = errorArray[0];
+		} else {
+			console.log('name');
+		}
+
+		if($scope.userEmail === '' || angular.isUndefined($scope.userEmail)) {
+			// console.log('empty email');
+			$scope.noEmailError = errorArray[1];
+		} else {
+			console.log('email');
+
+			for(var i=0; i < $scope.userEmail.length; i++) {
+				if($scope.userEmail.indexOf('@') === -1) {
+					console.log('no At sign');
+					$scope.noAtSignError = errorArray[2];
+				} else {
+					console.log('There is an At sign');
+				}
+			}
+		}
+
+		if(angular.isUndefined($scope.userWebsite) || $scope.userWebsite.substring(0, 7) !== 'http://') {
+			console.log('website undefined');
+			$scope.noWebError = errorArray[3];
+		} else {
+			console.log('http:// website');
+		}
+
+		if($scope.userMessage === '' || angular.isUndefined($scope.userMessage)) {
+			// console.log('empty message');
+			$scope.noMessageError = errorArray[4];
+		} else {
+			console.log('message');
 		}
 
 	}
 
 });
+
